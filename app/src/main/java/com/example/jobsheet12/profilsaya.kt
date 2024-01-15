c876f0b7c5a8761a533585336edd42a2527a0cab
adibmuzakki <adibmu>
2024-01-15 11:05:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jobsheet12/profilsaya.kt
@@ -0,0 +1,11 @@
+package com.example.jobsheet12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profilsaya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profilsaya)
+    }
+}
